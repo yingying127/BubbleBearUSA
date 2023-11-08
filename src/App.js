8be8cc8f8487ac6784566168bec99a10861019d9
Ying Ying <yingyingfengg@gmail.com>
2023-11-07 22:16:44 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Entering Bubble Bear Tea</h1>
+        <h3>Expanding the possibilities of tea. <br></br>Freshly brewed loose leaf tea paired with honey coated boba.</h3>
       </header>
     </div>
   );
