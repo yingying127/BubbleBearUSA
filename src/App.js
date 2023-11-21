35b775c99ced3501318ed165665414619902e42f
Ying Ying <yingyingfengg@gmail.com>
2023-11-20 21:30:41 -0500

    working onnavbar, added some react and installed tailwind and motion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import './App.css';
+import Navbar from "./scenes/Navbar";
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import { motion } from "framer-motion";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Entering Bubble Bear Tea</h1>
-        <h3>Expanding the possibilities of tea. <br></br>Freshly brewed loose leaf tea paired with honey coated boba.</h3>
-      </header>
+    <div className="app">
+     <Navbar
+      isTopOfPage={isTopOfPage}
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+     />
+     <div className="w-5/6 mx-auto md:h-full">
+      {isDesktop}
+      {/* <motion.div
+        margin="0 0 -200px 0"
+        amount="all"
+        onViewportEnter={() => setSelectedPage="home")}
+      >
+
+      </motion.div> */}
+     </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/scenes/Navbar.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import useMediaQuery from "../hooks/useMediaQuery"; 
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>hey</h1>
+        </div>
+    )
+}
+
+export default Navbar;
