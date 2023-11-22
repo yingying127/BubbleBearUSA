d8444b7daf403b067336cba797409192b4039121
Ying Ying <yingyingfengg@gmail.com>
2023-11-21 22:15:32 -0500

    changed green bg

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -4,17 +4,12 @@ const Home = ( {setSelectedPage }) => {
     const isAboveLarge = useMediaQuery("(min-width: 1060px)");
 
     return (
-        <div
-            id="home"
-            className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
-        >
-            <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
+        <div>
+            <div>
                 {isAboveLarge ? (
-                    <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px]
-                    before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+                    <div>
                         <img
-                            alt="profile"
-                            className="hover:filter transition duration-500 z-10 w-full max-w-[300px] md:max-w-[500px]"
+                            alt="home-bg"
                             src="assets/home-landing.png"
                         />
                     </div>
